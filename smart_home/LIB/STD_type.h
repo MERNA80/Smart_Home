e389f225b25bf1a49fdf22416eae8c4cace839f7
Merna Ahmed <mernnaahmed45@gmail.com>
2026-09-22 20:25:45 +0300

    Update STD_type.h

--- smart_home/LIB/STD_type.h
+++ smart_home/LIB/STD_type.h
@@ -8,20 +8,25 @@
 #ifndef REPO_LIB_STD_TYPE_H_
 #define REPO_LIB_STD_TYPE_H_
 
-typedef unsigned char uint8 ;
+typedef unsigned char  uint8  ;
 typedef unsigned short uint16 ;
-typedef unsigned int uint32 ;
-typedef unsigned long uint64 ;
+typedef unsigned int   uint32 ;
+typedef unsigned long  uint64 ;
 
-typedef signed char sint8 ;
-typedef signed short sint16 ;
-typedef signed int sint32 ;
-typedef signed long sint64 ;
+typedef signed char    sint8  ;
+typedef signed short   sint16 ;
+typedef signed int     sint32 ;
+typedef signed long    sint64 ;
+
+typedef float                  f32 ;
+typedef double                 f64 ;
 
 typedef enum
 {
 	Low,
 	High
 }STD_LevelType;
+
+
 
 #endif /* REPO_LIB_STD_TYPE_H_ */
