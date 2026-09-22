30e2e27871c7f030f3b4c5544e2ec4b7f1ed7ebb
Mernna08 <mernnaahmed45@gmail.com>
2026-09-22 19:59:23 +0300

    Add files via upload

---
+++ LIB/STD_type.h
@@ -0,0 +1,27 @@
+/*
+ * STD_type.h
+ *
+ *  Created on: Sep 19, 2026
+ *      Author: Mernna Ahmed
+ */
+
+#ifndef REPO_LIB_STD_TYPE_H_
+#define REPO_LIB_STD_TYPE_H_
+
+typedef unsigned char uint8 ;
+typedef unsigned short uint16 ;
+typedef unsigned int uint32 ;
+typedef unsigned long uint64 ;
+
+typedef signed char sint8 ;
+typedef signed short sint16 ;
+typedef signed int sint32 ;
+typedef signed long sint64 ;
+
+typedef enum
+{
+	Low,
+	High
+}STD_LevelType;
+
+#endif /* REPO_LIB_STD_TYPE_H_ */
