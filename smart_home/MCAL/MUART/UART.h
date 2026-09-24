a3faf89d5fbad4e7566a754dab0d018ee81d3c66
ReemSami54 <sreem4448@gmail.com>
2026-09-24 11:33:31 +0300

    Add files via upload

---
+++ smart_home/MCAL/MUART/UART.h
@@ -0,0 +1,25 @@
+/*
+ * UART.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: sreem
+ */
+
+#ifndef REPO_MCAL_MUART_UART_H_
+#define REPO_MCAL_MUART_UART_H_
+
+
+#ifndef MCAL_MUART_UART_H_
+#define MCAL_MUART_UART_H_
+
+#define UDR        (*(volatile u8*)(0x2C))
+#define UCSRA      (*(volatile u8*)(0x2B))
+#define UCSRB      (*(volatile u8*)(0x2A))
+#define UCSRC      (*(volatile u8*)(0x40))
+#define UBRRL      (*(volatile u8*)(0x29))
+#define UBRRH      (*(volatile u8*)(0x40))
+
+void MUART_voidInit (u32 A_u32BaudRata , u8 A_u8DataSize) ;
+u8   MUART_u8Rx     (void) ;
+
+#endif /* REPO_MCAL_MUART_UART_H_ */
