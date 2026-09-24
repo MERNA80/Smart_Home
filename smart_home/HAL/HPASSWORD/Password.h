021834afd48306ec258260c394fdf931eddc3cdd
ReemSami54 <sreem4448@gmail.com>
2026-09-24 12:14:23 +0300

    Add files via upload

---
+++ smart_home/HAL/HPASSWORD/Password.h
@@ -0,0 +1,16 @@
+/*
+ * Password.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: sreem
+ */
+
+#ifndef REPO_HAL_HPASSWORD_PASSWORD_H_
+#define REPO_HAL_HPASSWORD_PASSWORD_H_
+
+#define Pass_Length 4
+
+void PASS_voidSave(u8* A_Pass);
+u8 PASS_u8Check(u8* A_Pass);
+
+#endif /* REPO_HAL_HPASSWORD_PASSWORD_H_ */
