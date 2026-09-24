ec0c2e5bf99ddf3458887651d09138cee2c9faf5
oomrash6 <oomrash6@gmail.com>
2026-09-24 17:38:27 +0300

    Change LM35_ADC_CHANNEL from 0 to 3

--- smart_home/HAL/HLM35/LM35.h
+++ smart_home/HAL/HLM35/LM35.h
@@ -10,7 +10,7 @@
 
 #include "../../LIB/STD_types.h"
 
-#define LM35_ADC_CHANNEL   0   /* PA0 (PA4~PA7 are used by the LCD) */
+#define LM35_ADC_CHANNEL   3   /* PA0 (PA4~PA7 are used by the LCD) */
 
 void HLM35_voidInit         (void) ;
 u8   HLM35_u8GetTemperature (void) ;
